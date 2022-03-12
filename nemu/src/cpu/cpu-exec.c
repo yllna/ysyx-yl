12634d623f36e89ce046d4bf52d4c2fde6bc8e5e
201220098-Yang Lin <201220098@smail.nju.edu.cn>
2022-03-13 00:58:46 +0800

    try to cp

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -23,6 +23,15 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 #endif
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
+   	
+	#ifdef CONFIG_WATCHPOINT
+	char *e = check_watch_point();
+	if (e)
+	{
+		nemu_state.state = NEMU_STOP;
+		printf("expression(%s)'s value was changed.\n", e);
+	}
+	 #endif
 }
 
 static void exec_once(Decode *s, vaddr_t pc) {
