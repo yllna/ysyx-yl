97425ceae70356e17b18381a714f43c8fe05e1b2
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-14 23:22:04 +0800

    >  compile NEMU ysyx_22040015 杨林 Linux younglim 5.16.13-arch1-1 #1 SMP PREEMPT Tue, 08 Mar 2022 20:07:36 +0000 x86_64 GNU/Linux  23:22:04 up  4:52,  5 users,  load average: 2.23, 1.56, 1.42

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -13,8 +13,88 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
   panic("Not implemented");
 }
 
-int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+int sprintf(char *out, const char *fmt, ...)
+{
+	va_list ap;
+	size_t out_idx = 0;
+	int d;
+	// char c;
+	char *s;
+	int counter = 0;
+	va_start(ap, fmt);
+	while (*fmt)
+	{
+		if ((*fmt) == '%')
+		{
+			fmt++;
+			switch (*fmt)
+			{
+			case 's': /* string */
+				s = va_arg(ap, char *);
+				while (s)
+				{
+					out[out_idx++] = *s++;
+				}
+				counter++;
+				break;
+			case 'd': /* int */
+				d = va_arg(ap, int);
+				char number[15] = {0};
+				int number_idx = 0;
+				if (d == 0)
+				{
+					out[out_idx++] = '0';
+				}
+				else if((unsigned)d == 0x80000000){
+					out[out_idx++] = '-';
+					unsigned d_ = d;
+					while (d_)
+					{
+						number[number_idx++] = (d_ % 10) + '0';
+						d_ /= 10;
+					}
+					while (number_idx)
+					{
+						out[out_idx++] = number[number_idx--];
+					}
+				}
+				else
+				{
+					if (d < 0)
+					{
+						out[out_idx++] = '-';
+						d = -d;
+					}
+					while (d)
+					{
+						number[number_idx++] = (d % 10) + '0';
+						d /= 10;
+					}
+					while (number_idx)
+					{
+						out[out_idx++] = number[number_idx--];
+					}
+				}
+				counter++;
+				break;
+				// case 'c': /* char */
+				// 	/* need a cast here since va_arg only
+				//           takes fully promoted types */
+				// 	c = (char)va_arg(ap, int);
+				// 	break;
+			default:
+				out[out_idx++] = *fmt;
+			}
+			fmt++;
+		}
+		else
+		{
+			out[out_idx++] = *fmt;
+			fmt++;
+		}
+	}
+	va_end(ap);
+	return counter;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -4,44 +4,108 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-size_t strlen(const char *s) {
-  panic("Not implemented");
+size_t strlen(const char *s)
+{
+	size_t i = 0;
+	while (*s)
+	{
+		i++, s++;
+	}
+	return i;
 }
 
-char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+char *strcpy(char *dst, const char *src)
+{
+	for (size_t i = 0; src[i]; i++)
+	{
+		dst[i] = src[i];
+	}
+	return dst;
 }
 
-char *strncpy(char *dst, const char *src, size_t n) {
-  panic("Not implemented");
+char *strncpy(char *dst, const char *src, size_t n)
+{
+	size_t i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dst[i] = src[i];
+	for (; i < n; i++)
+		dst[i] = '\0';
+	return dst;
 }
 
-char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+char *strcat(char *dst, const char *src)
+{
+	size_t dst_len = strlen(dst);
+	size_t i;
+	for (i = 0; src[i] != '\0'; i++)
+		dst[dst_len + i] = src[i];
+	
+	dst[dst_len + i] = '\0';
+
+	return dst;
 }
 
-int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+int strcmp(const char *s1, const char *s2)
+{
+	for(size_t i = 0; s1[i] && s2[i] ;i++){
+		if(s1[i] != s2[i]){
+			return s1[i] - s2[i];
+		}
+	}
+	return 0;
 }
 
-int strncmp(const char *s1, const char *s2, size_t n) {
-  panic("Not implemented");
+int strncmp(const char *s1, const char *s2, size_t n)
+{
+	for(size_t i = 0; i < n ;i++){
+		if(s1[i] != s2[i]){
+			return s1[i] - s2[i];
+		}
+	}
+	return 0;
 }
 
-void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+void *memset(void *s, int c, size_t n)
+{
+	
+	for(size_t i = 0; i < n; i++){
+		*(((uint8_t *)s) + i) = (unsigned char )c;
+	}
+	return s;
 }
 
-void *memmove(void *dst, const void *src, size_t n) {
-  panic("Not implemented");
+void *memmove(void *dst, const void *src, size_t n)
+{
+	uint8_t temp[n];
+	for(size_t i = 0; i < n; i++){
+		temp[i] = 0;
+	}
+	for(size_t i = 0; i < n; i++){
+		temp[i] = *(((uint8_t *) src) + i);
+	} 
+	for(size_t i = 0; i < n; i++){
+		*(((uint8_t *)dst) + i) = temp[i];
+	}
+	return dst;
 }
 
-void *memcpy(void *out, const void *in, size_t n) {
-  panic("Not implemented");
+void *memcpy(void *out, const void *in, size_t n)
+{
+	for(size_t i = 0; i < n; i++){
+		*(((uint8_t *)out) + i) = *(((uint8_t *)in) + i);
+	}
+	return out;
 }
 
-int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+int memcmp(const void *s1, const void *s2, size_t n)
+{
+	for(size_t i = 0; i < n ;i++){
+		if(*(((uint8_t *)s1) + i) != *(((uint8_t *)s2)+ i)){
+			return *(((uint8_t *)s1) + i) - *(((uint8_t *)s2)+ i);
+		}
+	}
+	return 0;
 }
 
 #endif
