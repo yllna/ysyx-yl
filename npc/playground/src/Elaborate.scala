32f52c4c8fc1362a7b8df77d2dc079753a6deedd
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-02 17:33:55 +0800

    >  compile NEMU ysyx_22040294 杨林 Linux younglim 5.17.4-arch1-1 #1 SMP PREEMPT Wed, 20 Apr 2022 18:29:28 +0000 x86_64 GNU/Linux  17:33:55 up 1 day,  3:06,  3 users,  load average: 0.67, 0.68, 0.66

--- npc/playground/src/Elaborate.scala
+++ npc/playground/src/Elaborate.scala
@@ -1,4 +1,5 @@
 import chisel3._
 object Elaborate extends App {
-  (new chisel3.stage.ChiselStage).execute(args, Seq( chisel3.stage.ChiselGeneratorAnnotation( () => new Adder() ) )  )
+  // (new chisel3.stage.ChiselStage).execute(args, Seq( chisel3.stage.ChiselGeneratorAnnotation( () => new Npc() ) )  )
+   (new chisel3.stage.ChiselStage).execute(args, Seq( chisel3.stage.ChiselGeneratorAnnotation( () => new PcRegister(64) ) )  )
 }
--- npc/playground/src/Npc.scala
+++ npc/playground/src/Npc.scala
@@ -14,21 +14,33 @@ class Npc extends Module {
 }
 
 
-class RegisterFile(val bit_size : Int)extends Module{
+class RegisterFile(val word : Int)extends Module{
 	val io = IO(new Bundle{
 		val r1 = Input(UInt(5.W))
 		val r2 = Input(UInt(5.W))
 		val rd = Input(UInt(5.W))
 		val we = Input(Bool())
-		val rd_data = Input(UInt(bit_size.W))
-		val r1_data = Output(UInt(bit_size.W))
-		val r2_data = Output(UInt(bit_size.W))
+		val rd_data = Input(UInt(word.W))
+		val r1_data = Output(UInt(word.W))
+		val r2_data = Output(UInt(word.W))
 	})
-	val reg_vec = RegInit(VecInit(Seq.fill(32)(0.U(bit_size.W))))
+	val reg_vec = RegInit(VecInit(Seq.fill(32)(0.U(word.W))))
 	io.r1_data := reg_vec(io.r1)
 	io.r2_data := reg_vec(io.r2)
 	when(io.we){
-		reg_vec(io.rd) := Mux(io.rd === 0.U(5.W), 0.U(bit_size.W), io.rd_data)
+		reg_vec(io.rd) := Mux(io.rd === 0.U(5.W), 0.U(word.W), io.rd_data)
 	}
-	
 }
+
+class PcRegister(val word: Int) extends Module{
+	val io = IO(new Bundle{
+		val next_pc = Input(UInt(word.W))
+		val curr_pc = Output(UInt(word.W))
+	})
+	val pc = RegInit(UInt(word.W), 0x80000000L.U)
+	pc := io.next_pc
+	io.curr_pc := pc
+}
+
+
+
